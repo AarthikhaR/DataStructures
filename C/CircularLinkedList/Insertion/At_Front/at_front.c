a446aabda4bafc2226050dc6d402764f2df78f4b
AarthikhaR <103199839+AarthikhaR@users.noreply.github.com>
2023-02-08 22:08:02 +0530

    Create at_front.c

---
+++ C/CircularLinkedList/Insertion/At_Front/at_front.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+}Node;
+
+Node * push_beg(Node **headref)
+{
+    int new_data;
+    printf("\nEnter the data to be inserted = ");
+    scanf("%d",&new_data);
+    
+    Node * new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = new_data;
+   
+     
+    if(*headref == NULL)
+    {
+        *headref = new_node;
+        new_node -> next = *headref;
+    }
+    
+    else
+    {
+        Node *temp = *headref;
+        while(temp -> next != *headref)
+            temp = temp->next;
+        new_node -> next = *headref;
+        temp -> next = new_node;
+        *headref = new_node;
+    }
+    return *headref;
+}
+
+
+void displayList(Node * head)
+{
+    int n=1;
+    Node * t = head;
+    printf("\n");
+    do
+    {
+        printf("\nnode %d : %d ",n,t->data);
+        t = t->next;
+        n++;
+    }while(t != head);
+    printf("\n");
+}
+int main()
+{
+    Node * head = NULL;
+    int n,i,value;
+    printf("\nHow many nodes do u want to insert = ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    	head = push_beg(&head);
+    displayList(head);
+}
